57113c185ff19ac43a72051d92e2c8f19718c0c4
seunyboyxu <seunyboy09@gmail.com>
2023-02-18 16:38:04 +0000

    made a function to be able to make an access file

--- UI 21/login.cs
+++ UI 21/login.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data.OleDb;
 using System.Security.Cryptography;
 using Microsoft.Office.Interop.Excel;
+using ADOX;
 
 namespace UI_21
 {
@@ -23,33 +24,60 @@ namespace UI_21
     class login
     {
         //created temporary text boxes
-        System.Windows.Forms.TextBox txtRegisterPassword = new System.Windows.Forms.TextBox();
-        System.Windows.Forms.TextBox txtRegisterUsername = new System.Windows.Forms.TextBox();
-        System.Windows.Forms.TextBox txtLoginPassword = new System.Windows.Forms.TextBox();
-        System.Windows.Forms.TextBox txtLoginUsername = new System.Windows.Forms.TextBox();
+        public System.Windows.Forms.TextBox txtRegisterPassword = new System.Windows.Forms.TextBox();
+        public System.Windows.Forms.TextBox txtRegisterUsername = new System.Windows.Forms.TextBox();
+        public System.Windows.Forms.TextBox txtLoginPassword = new System.Windows.Forms.TextBox();
+        public System.Windows.Forms.TextBox txtLoginUsername = new System.Windows.Forms.TextBox();
 
 
         private static OleDbConnection con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db_graphix_users.mdb"); //establishing connection
         private static OleDbCommand cmd = new OleDbCommand(); // command to execute later
         private static OleDbDataAdapter da = new OleDbDataAdapter(); //data adapter
 
-        private void btnRegister_Click(object sender, EventArgs e)  //register part there 
-        {
-            string hashedPassword = Hashing.HashPassword(txtRegisterPassword.Text);//hash
-            string regSQL = "INSERT INTO tbl_users ([username], [password]) VALUES ('" + txtRegisterUsername.Text + "', '" + hashedPassword + "')";//hash
+        public class Task
 
-            if (txtRegisterUsername.Text == "" && txtRegisterPassword.Text == "") //check if fields are empty
+        {
+            login Login = new login();
+            public static void CreateAccessFile(string filename)
+
+            {
+
+                if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "DataBase\\"))
+
+                {
+
+                    Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "DataBase\\");
+
+                }
+                
+                ADOX.CatalogClass cat = new CatalogClass();
+
+                string str = "provider=Microsoft.Jet.OleDb.4.0;Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "DataBase\\" + filename + ".mdb;";
+
+                cat.Create(str);
+
+                cat = null;
+
+            }
+
+            private void btnRegister_Click(object sender, EventArgs e)  //register part there 
+        {
+                
+            string hashedPassword = Hashing.HashPassword(Login.txtRegisterPassword.Text);//hash
+            string regSQL = "INSERT INTO tbl_users ([username], [password]) VALUES ('" + Login.txtRegisterUsername.Text + "', '" + hashedPassword + "')";//hash
+
+            if (Login.txtRegisterUsername.Text == "" && Login.txtRegisterPassword.Text == "") //check if fields are empty
             {
                 MessageBox.Show("Username and Password fields are empty.", "Registration failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else if (txtRegisterUsername.Text == "" || txtRegisterPassword.Text == "") //check if fields are empty
+            else if (Login.txtRegisterUsername.Text == "" || Login.txtRegisterPassword.Text == "") //check if fields are empty
             {
                 MessageBox.Show("Username or Password fields are empty.", "Registration failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
                 con.Open(); //open connection
-                regSQL = "INSERT INTO tbl_users ([username], [password]) VALUES ('" + txtRegisterUsername.Text + "', '" + txtRegisterPassword.Text + "')"; //the sql command to insert username and password
+                regSQL = "INSERT INTO tbl_users ([username], [password]) VALUES ('" + Login.txtRegisterUsername.Text + "', '" + Login.txtRegisterPassword.Text + "')"; //the sql command to insert username and password
                 cmd = new OleDbCommand(regSQL, con); //setting up the command and the connection
                 try
                 {
@@ -63,8 +91,8 @@ namespace UI_21
                 catch (Exception ex) //catch if cmd does not execute
                 {
                     MessageBox.Show("Username has already been registered,", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtRegisterUsername.Clear(); //clear the fields
-                    txtRegisterPassword.Clear();
+                    Login.txtRegisterUsername.Clear(); //clear the fields
+                    Login.txtRegisterPassword.Clear();
                     con.Close(); //closing the connection or else there is an error
                 }
             }
@@ -73,13 +101,13 @@ namespace UI_21
         private void btnLogin_Click(object sender, EventArgs e) //login part
         {
 
-            string hashedPassword = Hashing.HashPassword(txtLoginPassword.Text);//hash
-            string logSQL = "SELECT * FROM tbl_users WHERE username=  '" + txtLoginUsername.Text + "' and password= '" + hashedPassword + "'";//hash
+            string hashedPassword = Hashing.HashPassword(Login.txtLoginPassword.Text);//hash
+            string logSQL = "SELECT * FROM tbl_users WHERE username=  '" + Login.txtLoginUsername.Text + "' and password= '" + hashedPassword + "'";//hash
 
             try
             {
                 con.Open(); //connection open
-                logSQL = "SELECT * FROM tbl_users WHERE username=  '" + txtLoginUsername.Text + "' and password= '" + txtLoginPassword.Text + "'"; //sql statement
+                logSQL = "SELECT * FROM tbl_users WHERE username=  '" + Login.txtLoginUsername.Text + "' and password= '" + Login.txtLoginPassword.Text + "'"; //sql statement
                 cmd = new OleDbCommand(logSQL, con); //execute the sql
 
                 OleDbDataReader dr = cmd.ExecuteReader(); //use the reader to read through the usernames and passwords
@@ -92,8 +120,8 @@ namespace UI_21
                 else //invalid login
                 {
                     MessageBox.Show("Invalid username or password.", "Login failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtLoginPassword.Clear();
-                    txtLoginUsername.Clear();
+                    Login.txtLoginPassword.Clear();
+                    Login.txtLoginUsername.Clear();
                     con.Close();
                 }
             }
